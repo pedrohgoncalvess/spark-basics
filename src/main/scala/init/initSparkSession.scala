a02990e1bad61ed0f7a1f648494e2b4e83238fcf
Pedro H <pedro_gonsalves@hotmail.com>
2023-03-15 00:37:30 -0300

    raw method to read csv files"

--- src/main/scala/init/initSparkSession.scala
+++ src/main/scala/init/initSparkSession.scala
@@ -17,14 +17,17 @@ object initSparkSession extends App {
 
     val dataRDD = ss.sparkContext.textFile(path)
 
-    val rowNum = dataRDD.count()
-    println(s"Number of rows: $rowNum")
-    dataRDD.take(15).foreach(println)
-
-    val header = dataRDD.first()
-    val csvRDDWithoutHeader = dataRDD.filter(_ != header)
+    val header = dataRDD.first() //HEADER OF DATASET
+    val csvRDDWithoutHeader = dataRDD.filter(_ != header) //REMOVE HEADER
     csvRDDWithoutHeader.take(10).foreach(println)
 
+    //val separetValues = csvRDDWithoutHeader.map(line => line.split(",")(0)) //SEPARATE DATASET WITH REGEX (0) IS INDEX OF COLUMN
+    //separetValues.take(50).foreach(println)
+
+    val separetColumns = csvRDDWithoutHeader.map(line => {
+      val colArray = line.split(",")
+      Array(colArray(0),colArray(1),colArray(2)).mkString(":") //mkString == separator value
+    }).take(10).foreach(println)
   }
   readCSVFile("C:\\Users\\Pedro\\Desktop\\WorkSpace\\Scala\\Datasets\\charts.csv")
 }
